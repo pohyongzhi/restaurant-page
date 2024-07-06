44e046ea7c1a2c3ae5c304b81c197e5601d53f79
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-07-06 18:16:20 +0800

    Added HtmlWebPackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -14,4 +15,10 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+        }),
+    ],
 };
